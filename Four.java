dfdfb0709b30d87b0ad8ee358d9f4ec680ea44d5
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-06-05 14:44:09 +0545

    practice question 4

---
+++ Four.java
@@ -0,0 +1,19 @@
+public class Four {
+
+    public static void main(String[] args)
+    {
+        int a = 0;
+        int b = 1;
+        int n = 10;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        for(int i=1;i<n-1;i++)
+        {
+            int c = a + b;
+            System.out.print(c+" ");
+            a = b;
+            b = c;
+        }
+    }
+    
+}
