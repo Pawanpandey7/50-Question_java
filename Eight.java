c060d735ff943776e84719abffcfb1ab83bb144e
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-06-06 14:55:40 +0545

    Practice qun eight

---
+++ Eight.java
@@ -0,0 +1,24 @@
+// gcd of two numbers
+import java.util.Scanner;
+public class Eight {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int n1 =sc.nextInt();
+        System.out.println("Enter the second number");
+        int n2 =sc.nextInt();
+        int greatest = 1;
+        for (int i = 1; i<=n1;i++)
+        {
+            if (n1%i==0 && n2%i==0){
+                greatest = i;
+            }
+        }
+        System.out.println("the gcd of "+n1+"and"+n2+"is"+greatest);
+
+
+    }
+
+    
+}
