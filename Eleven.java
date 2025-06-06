2b637138476fe7302a766144597cb7f90ba50c13
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-06-06 15:31:55 +0545

    largest element in the list

---
+++ Eleven.java
@@ -0,0 +1,25 @@
+//find the largest element in the list
+import java.util.List;
+import java.util.ArrayList;
+public class Eleven {
+    public static void main(String[] args)
+    {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(20);
+        numbers.add(30);
+        int n = numbers.size();
+        int greatest = numbers.get(0);
+        for(int i = 0;i<n;i++)
+        {
+            if(numbers.get(i)>greatest)
+            {
+                greatest = numbers.get(i);
+            }
+        }
+        System.out.print("the greatest number in the list is "+greatest);
+    }
+
+
+    
+}
