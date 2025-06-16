6c39d5fb15ed92213406e83e5a6f5ee702534aee
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-06-16 20:41:10 +0545

    sum of the elements of the array using recursion

---
+++ ThirtyFour.java
@@ -0,0 +1,22 @@
+
+public class ThirtyFour{
+    public static int Sum_array(int[] arr,int index)
+    {
+        
+        if(index==arr.length)
+        {
+            return 0;
+
+        }else{
+            return arr[index]+Sum_array(arr,index+1);
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3};
+        System.out.print(Sum_array(arr,0));
+
+
+
+    }
+}
